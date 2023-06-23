60c4855287519a3d26d09fd8386a045f4613515d
Jarmila <jarmila.kaska@gmail.com>
2023-06-23 14:25:45 +0200

    Duplicated characters recognition

--- 6_DuplicateEncoder.py
+++ 6_DuplicateEncoder.py
@@ -12,27 +12,14 @@
 # Assertion messages may be unclear about what they display in some languages. 
 # If you read "...It Should encode XXX", the "XXX" is the expected result, not the input!
 
+
 def duplicate_encode(word):
-    word_l = list(word)
-    pass
-
-
-code = 'hello'
-code_l = list(code)
-
-encoded = []
-
-for x in code_l:
-    l_list = []
-    while x in code_l:
-        l_list.append(x)
-        code_l.remove(x)
-        print(code_l)
-        print(l_list)
-        if len(l_list) == 1:
-            [encoded.append('(') for x in code_l]
-            # l_list.append(l)
-        if len(l_list) > 1:
-            [encoded.append(')') for x in code_l]
-            # l_list.append(l)
-print(encoded)
+    encoded = []
+    for letter in word:
+        letter_count = word.count(letter)
+        if letter_count == 1:
+            encoded.append('(')
+        else:
+            encoded.append(")")
+    encoded = ('').join(encoded)
+    return encoded
